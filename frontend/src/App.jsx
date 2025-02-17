8dd3da04be83bb39fd02710aa8ac97de970cff4f
Dhayanithi <545nithi@gmail..com>
2025-02-17 13:34:38 +0530

    Milestore 13 Completed

---
+++ frontend/src/App.jsx
@@ -0,0 +1,23 @@
+/* eslint-disable no-unused-vars */
+
+import './App.css';
+import React from 'react';
+import {Login,Signup,Home, CreateProduct, MyProducts} from "./Routes/Routes"
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+function App() {
+  return (
+    <BrowserRouter>
+    <Routes>
+    <Route path='/' element={<Home/>}/>
+    <Route path='/login' element={<Login/>}/>
+    <Route path='/create-user' element={<Signup/>}/>
+    <Route path='/create-product' element={<CreateProduct/>}/>  
+    {/* For edit product by id */}
+    <Route path="/create-product/:id" element={<CreateProduct />} />
+    <Route path="/my-products" element={<MyProducts/>} />
+    </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
